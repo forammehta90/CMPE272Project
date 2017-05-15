04d32b8716183bb50027359a897935a566de957a
foram mehta <forammehta90@gmail.com>
2017-05-15 06:46:02 -0700

    Irrigated Non-irrigated

--- python/request_json.py
+++ python/request_json.py
@@ -7,7 +7,7 @@ class request_json(object):
 
     def __init__(self):
         self.cli = sample.sample()
-        self.df = pd.read_csv('Texas.csv')
+        self.df = pd.read_csv('soybeans-texas-collin.csv')
         self.df.columns = [c.lower().replace(' ', '_') for c in self.df.columns]
         self.df['value'] = self.df['value'].str.replace(",","")
         self.df['value'] = self.df['value'].apply(pd.to_numeric, errors='coerce')
@@ -23,6 +23,15 @@ class request_json(object):
         columns.append({'key':'production', 'full_name':'Production', 'type':'numeric', 'goal':'max'})
         columns.append({'key':'acres_harvested', 'full_name':'Acres Harvested', 'type':'numeric', 'goal':'max'})
         columns.append({'key':'acres_planted', 'full_name':'Acres Planted', 'type':'numeric', 'goal':'max'})
+        columns.append({'key':'yield_irr', 'full_name':'Yield Irrigated', 'type':'numeric', 'goal':'max','is_objective':'false'})
+        columns.append({'key':'production_irr', 'full_name':'Production Irrigated', 'type':'numeric', 'goal':'max','is_objective':'false'})
+        columns.append({'key':'acres_harvested_irr', 'full_name':'Acres Harvested Irrigated', 'type':'numeric', 'goal':'max','is_objective':'false'})
+        columns.append({'key':'acres_planted_irr', 'full_name':'Acres Planted Irrigated', 'type':'numeric', 'goal':'max','is_objective':'false'})
+        columns.append({'key':'yield_nirr', 'full_name':'Yield Non Irrigated', 'type':'numeric', 'goal':'max','is_objective':'false'})
+        columns.append({'key':'production_nirr', 'full_name':'Production Non Irrigated', 'type':'numeric', 'goal':'max','is_objective':'false'})
+        columns.append({'key':'acres_harvested_nirr', 'full_name':'Acres Harvested Non Irrigated', 'type':'numeric', 'goal':'max','is_objective':'false'})
+        columns.append({'key':'acres_planted_nirr', 'full_name':'Acres Planted Non Irrigated', 'type':'numeric', 'goal':'max','is_objective':'false'})
+        columns.append({'key':'sales', 'full_name':'Sales', 'type':'numeric', 'goal':'max'})
         tempjson.update({'subject': 'Plantaplan'})
         tempjson.update({'columns': columns})
         for crop in crops:
--- python/sample.py
+++ python/sample.py
@@ -3,20 +3,57 @@
 import pandas as pd
 import json
 import json_maker
+import numpy as np
 
 class sample(object):
     def __init__(self):
         self.cli = json_maker.json_maker()
 
     def analyze(self, temp, stateName, countyName, cropName):
-        acres_harvested = temp[ (temp.commodity == cropName) & (temp.state == stateName) & (temp.county == countyName) & temp['data_item'].str.contains('ACRES HARVESTED')]
-        acres_planted = temp[ (temp.commodity == cropName) & (temp.state == stateName) & (temp.county == countyName) & temp['data_item'].str.contains('ACRES PLANTED')]
-        production = temp[ (temp.commodity == cropName) & (temp.state == stateName) & (temp.county == countyName) & temp['data_item'].str.contains('PRODUCTION')]
-        yield_measured = temp[ (temp.commodity == cropName) & (temp.state == stateName) & (temp.county == countyName) & temp['data_item'].str.contains('YIELD')]
+        acres_plant_str = cropName + " - ACRES PLANTED"
+        acres_harv_str = cropName + " - ACRES HARVESTED"
+        production_str = cropName + " - PRODUCTION, MEASURED IN BU"
+        yield_str = cropName + " - YIELD, MEASURED IN BU / ACRE"
+        sales_str = cropName + " - SALES, MEASURED IN $"
+        
+        acres_harvested = temp[ (temp.commodity == cropName) & (temp.state == stateName) & (temp.county == countyName) & (temp.data_item == acres_plant_str)]
+        acres_planted = temp[ (temp.commodity == cropName) & (temp.state == stateName) & (temp.county == countyName) & (temp.data_item == acres_harv_str)]
+        production = temp[ (temp.commodity == cropName) & (temp.state == stateName) & (temp.county == countyName) & (temp.data_item == production_str)]
+        yield_measured = temp[ (temp.commodity == cropName) & (temp.state == stateName) & (temp.county == countyName) & (temp.data_item == yield_str)]
+        sales = temp[ (temp.commodity == cropName) & (temp.state == stateName) & (temp.county == countyName) & (temp.data_item == sales_str)]
+        
+        acres_plant_irr_str = cropName + ", IRRIGATED - ACRES PLANTED"
+        acres_harv_irr_str = cropName + ", IRRIGATED - ACRES HARVESTED"
+        production_irr_str = cropName + ", IRRIGATED - PRODUCTION, MEASURED IN BU"
+        yield_irr_str = cropName + ", IRRIGATED - YIELD, MEASURED IN BU / ACRE"
+        acres_harvested_irr = temp[ (temp.commodity == cropName) & (temp.state == stateName) & (temp.county == countyName) & (temp.data_item == acres_plant_irr_str)]
+        acres_planted_irr = temp[ (temp.commodity == cropName) & (temp.state == stateName) & (temp.county == countyName) & (temp.data_item == acres_harv_irr_str)]
+        production_irr = temp[ (temp.commodity == cropName) & (temp.state == stateName) & (temp.county == countyName) & (temp.data_item == production_irr_str)]
+        yield_measured_irr = temp[ (temp.commodity == cropName) & (temp.state == stateName) & (temp.county == countyName) & (temp.data_item == yield_irr_str)]
+         
+        acres_plant_nirr_str = cropName + ", NON-IRRIGATED - ACRES PLANTED"
+        acres_harv_nirr_str = cropName + ", NON-IRRIGATED - ACRES HARVESTED"
+        production_nirr_str = cropName + ", NON-IRRIGATED - PRODUCTION, MEASURED IN BU"
+        yield_nirr_str = cropName + ", NON-IRRIGATED - YIELD, MEASURED IN BU / ACRE"
+        acres_harvested_nirr = temp[ (temp.commodity == cropName) & (temp.state == stateName) & (temp.county == countyName) & (temp.data_item == acres_plant_nirr_str)]
+        acres_planted_nirr = temp[ (temp.commodity == cropName) & (temp.state == stateName) & (temp.county == countyName) & (temp.data_item == acres_harv_nirr_str)]
+        production_nirr = temp[ (temp.commodity == cropName) & (temp.state == stateName) & (temp.county == countyName) & (temp.data_item == production_nirr_str)]
+        yield_measured_nirr = temp[ (temp.commodity == cropName) & (temp.state == stateName) & (temp.county == countyName) & (temp.data_item == yield_nirr_str)]
         list = {}
-        list.update({'acres_harvested': acres_harvested.value.mean()})
-        list.update({'acres_planted': acres_planted.value.mean()})
-        list.update({'production': production.value.mean()})
-        list.update({'yield': yield_measured.value.mean()})
+        
+        list.update({'acres_harvested': acres_harvested.value.mean().round(2)})
+        list.update({'acres_planted': acres_planted.value.mean().round(2)})
+        list.update({'production': production.value.mean().round(2)})
+        list.update({'yield': yield_measured.value.mean().round(2)})
+        list.update({'sales': sales.value.mean().round(2)})
+        list.update({'acres_harvested_irr': acres_harvested_irr.value.mean().round(2)})
+        list.update({'acres_planted_irr': acres_planted_irr.value.mean().round(2)})
+        list.update({'production_irr': production_irr.value.mean().round(2)})
+        list.update({'yield_irr': yield_measured_irr.value.mean().round(2)})
+        list.update({'acres_harvested_nirr': acres_harvested_nirr.value.mean().round(2)})
+        list.update({'acres_planted_nirr': acres_planted_nirr.value.mean().round(2)})
+        list.update({'production_nirr': production_nirr.value.mean().round(2)})
+        list.update({'yield_nirr': yield_measured_nirr.value.mean().round(2)})
+
         a = self.cli.prepare(list, cropName)
         return a
